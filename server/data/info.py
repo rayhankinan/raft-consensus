f5aaeee4e99296b2a4018f97e257c78ed101c5b6
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-05-18 21:32:30 +0700

    separate next and match Index for membership and state

--- server/data/info.py
+++ server/data/info.py
@@ -5,9 +5,17 @@ from . import Address
 class ServerInfo(NamedTuple):
     next_index: int
     match_index: int
+    state_next_index: int
+    state_match_index: int
 
     def get_next_index(self) -> int:
         return self.next_index
 
     def get_match_index(self) -> int:
         return self.match_index
+    
+    def get_state_next_index(self) -> int:
+        return self.state_next_index
+
+    def get_state_match_index(self) -> int:
+        return self.state_match_index
